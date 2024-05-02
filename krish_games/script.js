cd3a042cd2861edf49cdbbf30a57489dfa28c301
Navaneetha-Krishnan-S <krish.cgac@gmail.com>
2024-05-02 08:47:53 +0530

    initial commit

---
+++ krish_games/script.js
@@ -0,0 +1,11 @@
+
+var sideNavBar = document.querySelector(".side-navbar")
+function toggle(){
+    sideNavBar.style.left="0"
+}
+
+
+var sideNavBar = document.querySelector(".side-navbar")
+function toggleClose(){
+    sideNavBar.style.left="-70%"
+}
