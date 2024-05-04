6fbfc680fb14b2ad15f7e923dcac36a879c6e2f7
Navaneetha Krishnan S <157886085+Navaneetha-Krishnan-S@users.noreply.github.com>
2024-05-04 18:07:48 +0530

    Update collection.js

--- krish_fashion/collection.js
+++ krish_fashion/collection.js
@@ -1,3 +1,19 @@
+// navbar
+
+var sideNavBar = document.querySelector(".side-navbar")
+function toggle() {
+    sideNavBar.style.left = "0"
+}
+
+
+var sideNavBar = document.querySelector(".side-navbar")
+function toggleClose() {
+    sideNavBar.style.left = "-70%"
+}
+
+
+
+
 // search
 
 var container = document.getElementById("collection")
