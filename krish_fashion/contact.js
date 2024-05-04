3ad858690fcf2fc79cf0f28d0138db21c6d678d5
Navaneetha-Krishnan-S <krish.cgac@gmail.com>
2024-05-04 17:29:58 +0530

    completed

---
+++ krish_fashion/contact.js
@@ -0,0 +1,33 @@
+// contact alert
+
+var submit = document.getElementById("submit")
+
+
+submit.addEventListener("click", function (event) {
+    var name = document.getElementById("name")
+    var mail = document.getElementById("mail")
+    var message = document.getElementById("message")
+    event.preventDefault()
+
+
+    if (
+        name.value == "" ||
+        mail.value == "" ||
+        message.value == "") {
+        alert("Please fill all fields!")
+
+    }
+    else {
+        alert("Thank You for Contacting us! We will reply you soon.")
+    }
+
+    name.value = ""
+    mail.value = ""
+    message.value = ""
+
+
+})
+
+
+
+
